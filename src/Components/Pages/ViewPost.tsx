bde97bfb7767e946f237d0e1d8ef19f2933ba8d2
reiji <ivan2005verem@gmail.com>
2023-07-09 17:29:40 +0300

    init

---
+++ src/Components/Pages/ViewPost.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import Header from '../Header/Header'
+import View from '../View/View'
+
+function ViewPost() {
+    return (
+        <div>
+            <Header  />
+            <View/>
+        </div>
+    )
+}
+
+export default ViewPost
---
+++ src/Components/Route/MainRoutes.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import {BrowserRouter as Router , Route, Routes} from 'react-router-dom'
+import Home from '../Pages/Home'
+import Signup from '../Pages/Signup'
+import Login from '../Pages/Login'
+import ViewPost from '../Pages/ViewPost'
+import Create from '../Pages/Create'
+
+function MainRoutes() {
+    return (
+        
+        <Routes>
+          <Route  path="/"  Component={Home} />
+          <Route path="/signup"  Component={Signup} ></Route>
+          <Route path="/login"  Component={Login}></Route>
+          <Route path="/create"  Component={Create}></Route>
+          <Route path="/view-post" Component={ViewPost} />
+        </Routes>
+      
+    )
+}
+
+export default MainRoutes
