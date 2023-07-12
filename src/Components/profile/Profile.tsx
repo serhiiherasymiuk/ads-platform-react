bb91d6d2f802f00880ed47dd8c48c17b0ba75841
SerhiiHerasymiuk <@DESKTOP-8DFR7S5>
2023-07-12 23:24:35 +0300

    Added Profile

--- src/Components/profile/Profile.tsx
+++ src/Components/profile/Profile.tsx
@@ -3,6 +3,7 @@ import "./Profile.scss";
 import { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { IAuthUser } from "../../interfaces/user";
+import React from "react";
 export const Profile = () => {
   const navigate = useNavigate();
   const { user, isAuth } = useSelector((store: any) => store.auth as IAuthUser);
@@ -30,7 +31,7 @@ export const Profile = () => {
             </button>
           </Link>
         </div>
-        <div>
+        <div className="personal-info">
           <div>
             <h3>Personal Information</h3>
             <Link to="edit">
@@ -38,6 +39,20 @@ export const Profile = () => {
                 Edit<i className="bi bi-pencil"></i>
               </button>
             </Link>
+          </div>
+          <div>
+            <div>
+              <h5>Email</h5>
+              <p>{user?.email}</p>
+            </div>
+            <div>
+              <h5>Roles</h5>
+              <div className="role-list">
+                {user?.roles.map((r: any, id) => {
+                  return <p key={id}>{r}</p>;
+                })}
+              </div>
+            </div>
           </div>
         </div>
       </div>
