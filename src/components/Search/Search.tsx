ed665a7a06445a405704de77f016f4de735cb55d
Serhii Herasymiuk <sgerasimuk07@gmail.com>
2023-07-25 21:48:54 +0300

    Added subcategory filter

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,33 +1,42 @@
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import "./Search.scss";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Header } from "../home/header/Header";
 import { SearchSection } from "./searchSection/SearchSection";
 import { ProfileContainer } from "../home/profileContainer/ProfileContainer";
 import { IAdvertisement } from "../../interfaces/advertisement";
 import http_common from "../../http_common";
+import { ICategory } from "../../interfaces/category";
 
 export const Search = () => {
   const { value, category } = useParams();
 
   const [advertisements, setAdvertisements] = useState<IAdvertisement[]>([]);
 
+  const [headCategories, setHeadCategories] = useState<ICategory[]>([]);
+
+  const [allCategories, setAllCategories] = useState<ICategory[]>([]);
+
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  const selectRef = useRef<HTMLDivElement>(null);
+
   useEffect(() => {
     if (value) {
       if (category) {
         http_common
-          .get(`api/Advertisements/SearchByCategory/${value}/${category}`)
+          .get(`api/Advertisements/searchByCategory/${value}/${category}`)
           .then((resp) => {
             setAdvertisements(resp.data);
           });
       } else {
-        http_common.get(`api/Advertisements/Search/${value}`).then((resp) => {
+        http_common.get(`api/Advertisements/search/${value}`).then((resp) => {
           setAdvertisements(resp.data);
         });
       }
     } else if (category) {
       http_common
-        .get(`api/Advertisements/getByCategoryName/${category}`)
+        .get(`api/Advertisements/searchByCategory/${category}`)
         .then((resp) => {
           setAdvertisements(resp.data);
         });
@@ -36,16 +45,100 @@ export const Search = () => {
         setAdvertisements(resp.data);
       });
     }
+    fetchCategories();
+
+    const handleDocumentClick = (event: MouseEvent) => {
+      if (
+        selectRef.current &&
+        !selectRef.current.contains(event.target as Node)
+      ) {
+        setIsDropdownOpen(false);
+      }
+    };
+
+    document.addEventListener("click", handleDocumentClick);
+
+    return () => {
+      document.removeEventListener("click", handleDocumentClick);
+    };
   }, [value, category]);
 
+  const fetchCategories = () => {
+    if (headCategories) {
+      http_common
+        .get(`api/Categories/getByParentName/${category}`)
+        .then((resp) => {
+          setHeadCategories(resp.data);
+        });
+    }
+    http_common.get("api/Categories").then((resp) => {
+      setAllCategories(resp.data);
+    });
+  };
+
   const formatDate = (date: Date) => {
-    console.log(date);
     const options: Intl.DateTimeFormatOptions = {
       day: "2-digit",
       month: "long",
       year: "numeric",
     };
     return new Date(date).toLocaleString("en-US", options);
+  };
+
+  const [subcategories, setSubcategories] = useState<{
+    [parentId: number]: ICategory[];
+  }>({});
+
+  const [hoveredCategories, setHoveredCategories] = useState<number[]>([]);
+
+  const handleSubcategoryMouseEnter = (id: number) => {
+    setHoveredCategories((prevHoveredCategories) => [
+      ...prevHoveredCategories,
+      id,
+    ]);
+    if (!subcategories[id]) {
+      fetchSubcategories(id);
+    }
+  };
+
+  const handleSubcategoryMouseLeave = (id: number) => {
+    setHoveredCategories((prevHoveredCategories) =>
+      prevHoveredCategories.filter((categoryId) => categoryId !== id)
+    );
+  };
+
+  const fetchSubcategories = (id: number) => {
+    setSubcategories((prevSubcategories) => ({
+      ...prevSubcategories,
+      [id]: allCategories.filter((c) => c.parentId === id),
+    }));
+  };
+
+  const renderSubcategories = (id: number) => {
+    if (hoveredCategories.includes(id) && subcategories[id]?.length) {
+      return (
+        <div className="subcategory">
+          {subcategories[id].map((subcategory: ICategory) => (
+            <div
+              key={subcategory.id}
+              onMouseEnter={() => handleSubcategoryMouseEnter(subcategory.id)}
+              onMouseLeave={() => handleSubcategoryMouseLeave(subcategory.id)}
+            >
+              <Link to={`/${subcategory.name}`}>
+                <p>
+                  {subcategory.name}
+                  {allCategories.some((c) => c.parentId === subcategory.id) ? (
+                    <i className="bi bi-caret-right-fill"></i>
+                  ) : null}
+                </p>
+              </Link>
+              {renderSubcategories(subcategory.id)}
+            </div>
+          ))}
+        </div>
+      );
+    }
+    return null;
   };
 
   return (
@@ -56,6 +149,47 @@ export const Search = () => {
           <ProfileContainer></ProfileContainer>
         </div>
         <SearchSection></SearchSection>
+        <div className="filters">
+          <div
+            ref={selectRef}
+            className={`select ${isDropdownOpen ? "open" : ""}`}
+          >
+            <p onClick={() => setIsDropdownOpen((prevState) => !prevState)}>
+              Select a subcategory
+              <i className="bi bi-caret-down-fill"></i>
+            </p>
+            {isDropdownOpen &&
+              headCategories.map((category: ICategory) => {
+                return (
+                  <div
+                    onClick={() => {
+                      setIsDropdownOpen((prevState) => !prevState);
+                      setHoveredCategories([]);
+                    }}
+                    key={category.id}
+                    onMouseEnter={() =>
+                      handleSubcategoryMouseEnter(category.id)
+                    }
+                    onMouseLeave={() =>
+                      handleSubcategoryMouseLeave(category.id)
+                    }
+                  >
+                    <Link to={`/${category.name}`}>
+                      <p>
+                        {category.name}
+                        {allCategories.some(
+                          (c) => c.parentId === category.id
+                        ) ? (
+                          <i className="bi bi-caret-right-fill"></i>
+                        ) : null}
+                      </p>
+                    </Link>
+                    {renderSubcategories(category.id)}
+                  </div>
+                );
+              })}
+          </div>
+        </div>
         <div className="advertisements">
           {advertisements.map((c: IAdvertisement) => {
             return (
@@ -77,7 +211,7 @@ export const Search = () => {
                   </div>
                   <div>
                     <p>
-                      {c.location} {formatDate(c.creationDate)}
+                      {c.location} - {formatDate(c.creationDate)}
                     </p>
                   </div>
                 </div>
